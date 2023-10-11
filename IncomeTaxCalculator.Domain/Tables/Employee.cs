450dcd60d795d13dcae5846bc83243b7d2706997
Charles.Walker <charles.walker@blinds-2go.co.uk>
2023-10-11 12:09:52 +0100

    Adds skeleton test projects.

---
+++ IncomeTaxCalculator.Domain/Tables/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace IncomeTaxCalculator.Domain.Tables;
+
+public class Employee
+{
+    public int EmployeeId { get; set; }
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public DateOnly BirthDate { get; set; }
+    public decimal AnnualIncome { get; set; }
+}
--- IncomeTaxCalculator.Infrastructure/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace IncomeTaxCalculator.Infrastructure;
-
-public class Class1
-{
-}
